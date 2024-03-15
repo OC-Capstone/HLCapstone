25b4816e993afc66fc5a4007f281c6449f72697c
Lucas Benko <benko.lucas@gmail.com>
2024-03-15 00:15:58 -0700

    Modified logout function, cookies, login time

--- src/dashboard.php
+++ src/dashboard.php
@@ -1,3 +1,10 @@
+<?php
+if (!isset($_COOKIE['email'])) {
+    header("Location: login.html");
+    exit();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,66 +16,70 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.0.2/dist/tailwind.min.css" rel="stylesheet">
     <style>
-body {
-  font-family: "Lato", sans-serif;
-}
+        body {
+            font-family: "Lato", sans-serif;
+        }
 
-.sidenav {
-  height: 100%;
-  width: 0;
-  position: fixed;
-  z-index: 1;
-  top: 0;
-  left: 10;
-  background-color: #fff;
-  overflow-x: hidden;
-  transition: 0.5s;
-  /* padding-top: 60px; */
-}
+        .sidenav {
+            height: 100%;
+            width: 0;
+            position: fixed;
+            z-index: 1;
+            top: 0;
+            left: 10;
+            background-color: #fff;
+            overflow-x: hidden;
+            transition: 0.5s;
+            /* padding-top: 60px; */
+        }
 
-.sidenav a {
-  /* padding: 8px 8px 8px 32px; */
-  text-decoration: none;
-  font-size: 25px;
-  /* color: #818181; */
-  display: block;
-  transition: 0.3s;
-}
+        .sidenav a {
+            /* padding: 8px 8px 8px 32px; */
+            text-decoration: none;
+            font-size: 25px;
+            /* color: #818181; */
+            display: block;
+            transition: 0.3s;
+        }
 
-.sidenav a:hover {
-  color: #f1f1f1;
-}
+        .sidenav a:hover {
+            color: #f1f1f1;
+        }
 
-.sidenav .closebtn {
-  position: absolute;
-  top: 0;
-  right: 25px;
-  font-size: 36px;
-  margin-left: 50px;
-}
+        .sidenav .closebtn {
+            position: absolute;
+            top: 0;
+            right: 25px;
+            font-size: 36px;
+            margin-left: 50px;
+        }
 
-@media screen and (max-height: 450px) {
-  .sidenav {padding-top: 15px;}
-  .sidenav a {font-size: 18px;}
-}
-</style>
+        @media screen and (max-height: 450px) {
+            .sidenav {
+                padding-top: 15px;
+            }
+
+            .sidenav a {
+                font-size: 18px;
+            }
+        }
+    </style>
 </head>
 
 <body>
     <div id="mySidenav" class="sidenav">
-	<?php
-	include("navbar.html");
-	?>
-</div>    
+        <?php
+        include("navbar.html");
+        ?>
+    </div>
     <div class="fixed top-0 left-0 w-full h-16 bg-red-600 flex justify-between items-center px-4">
-    <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776;</span>
+        <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776;</span>
 
-        <h1 id="title" class="text-white text-4xl"
-            style="font-family: 'Lato', sans-serif; text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;">
+        <h1 id="title" class="text-white text-4xl" style="font-family: 'Lato', sans-serif; text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;">
             Secure Document Vault</h1>
 
 
-        <a href="#">
+        <a href="logout.php">
             <i class="fas fa-sign-out-alt text-white text-4xl"></i>
         </a>
     </div>
@@ -83,8 +94,56 @@ body {
 
                 <p id="lastUpdated" class="text-3xl mt-4 mb-12" style="font-family: 'Lato', sans-serif;">Last Updated:
                 </p>
-                <img src="../res/Vector.png" alt="logo" class="mx-auto"
-                    style="max-width: 400px; width: 100%; max-height: 350px; height: 100%">
+                <p id="lastUpdated" class="text-3xl mt-4 mb-12" style="font-family: 'Lato', sans-serif;">Last Login:
+                    <?php
+                    if (isset($_COOKIE['last_login'])) {
+                        $last_login = DateTime::createFromFormat('Y-m-d H:i:s', $_COOKIE['last_login']);
+                        if ($last_login instanceof DateTime) {
+                            $now = new DateTime('now', new DateTimeZone('UTC')); // Current date and time in UTC
+                            $now = DateTime::createFromFormat('Y-m-d H:i:s', $now->format('Y-m-d H:i:s'));
+                            $interval = $last_login->diff($now); // Difference between dates
+                            if ($interval->y > 0) {
+                                if ($interval->y == 1) {
+                                    echo $interval->y . " year ago";
+                                } else {
+                                    echo $interval->y . " years ago";
+                                }
+                            } else if ($interval->m > 0) {
+                                if ($interval->m == 1) {
+                                    echo $interval->m . " month ago";
+                                } else {
+                                    echo $interval->m . " months ago";
+                                }
+                            } else if ($interval->days > 0) {
+                                if ($interval->days == 1) {
+                                    echo $interval->days . " day ago";
+                                } else {
+                                    echo $interval->days . " days ago";
+                                }
+                            } elseif ($interval->h > 0) {
+                                if ($interval->h == 1) {
+                                    echo $interval->h . " hour ago";
+                                } else {
+                                    echo $interval->h . " hours ago";
+                                }
+                            } else if ($interval->i > 0) {
+                                if ($interval->i == 1) {
+                                    echo $interval->i . " minute ago";
+                                } else {
+                                    echo $interval->i . " minutes ago";
+                                }
+                            } else {
+                                echo $interval->s . " seconds ago";
+                            }
+                        } else {
+                            echo "Failed to parse last login date!";
+                        }
+                    } else {
+                        echo "No last login information found.";
+                    }
+                    ?>
+                </p>
+                <img src="../res/Vector.png" alt="logo" class="mx-auto" style="max-width: 400px; width: 100%; max-height: 350px; height: 100%">
             </div>
 
 
@@ -95,8 +154,7 @@ body {
 
 
                     <li class="relative">
-                        <a href="messages.html" class="text-black-500 hover:text-blue-700 text-2xl"
-                            style="font-family: 'Lato', sans-serif; white-space: nowrap;">Parting messages</a>
+                        <a href="messages.html" class="text-black-500 hover:text-blue-700 text-2xl" style="font-family: 'Lato', sans-serif; white-space: nowrap;">Parting messages</a>
                         <h1 class="text-5xl absolute left-1/2 transform -translate-x-1/2 top-12 -mt-2">7</h1>
 
                     </li>
@@ -104,8 +162,7 @@ body {
 
 
                     <li class="relative">
-                        <a href="pets.html" class="text-black-500 hover:text-blue-700 text-2xl"
-                            style="font-family: 'Lato', sans-serif; white-space: nowrap;">Pet wishes</a>
+                        <a href="pets.html" class="text-black-500 hover:text-blue-700 text-2xl" style="font-family: 'Lato', sans-serif; white-space: nowrap;">Pet wishes</a>
                         <h1 class="text-5xl absolute left-1/2 transform -translate-x-1/2 top-12 -mt-2">&#10003;</h1>
 
                     </li>
@@ -113,8 +170,7 @@ body {
 
 
                     <li class="relative">
-                        <a href="assets.html" class="text-black-500 hover:text-blue-700 text-2xl"
-                            style="font-family: 'Lato', sans-serif; white-space: nowrap;">Asset Inventory</a>
+                        <a href="assets.html" class="text-black-500 hover:text-blue-700 text-2xl" style="font-family: 'Lato', sans-serif; white-space: nowrap;">Asset Inventory</a>
                         <h1 class="text-5xl absolute left-1/2 transform -translate-x-1/2 top-12 -mt-2">&#10003;</h1>
 
                     </li>
@@ -122,8 +178,7 @@ body {
 
 
                     <li class="relative">
-                        <a href="wishes.html" class="text-black-500 hover:text-blue-700 text-2xl"
-                            style="font-family: 'Lato', sans-serif; white-space: nowrap;">Testamentary wishes</a>
+                        <a href="wishes.html" class="text-black-500 hover:text-blue-700 text-2xl" style="font-family: 'Lato', sans-serif; white-space: nowrap;">Testamentary wishes</a>
                         <h1 class="text-5xl absolute left-1/2 transform -translate-x-1/2 top-12 -mt-2">&#10003;</h1>
 
                     </li>
@@ -131,8 +186,7 @@ body {
 
 
                     <li class="relative">
-                        <a href="funeral.html" class="text-black-500 hover:text-blue-700 text-2xl"
-                            style="font-family: 'Lato', sans-serif; white-space: nowrap;">Funeral preferences</a>
+                        <a href="funeral.html" class="text-black-500 hover:text-blue-700 text-2xl" style="font-family: 'Lato', sans-serif; white-space: nowrap;">Funeral preferences</a>
                         <h1 class="text-5xl absolute left-1/2 transform -translate-x-1/2 top-12-mt-2">&#10003;</h1>
 
                     </li>
@@ -145,15 +199,15 @@ body {
 
 
     <script>
-function openNav() {
-  document.getElementById("mySidenav").style.width = "400px";
-}
+        function openNav() {
+            document.getElementById("mySidenav").style.width = "400px";
+        }
 
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-}
-</script>
-   
+        function closeNav() {
+            document.getElementById("mySidenav").style.width = "0";
+        }
+    </script>
+
 </body>
 
 </html>
